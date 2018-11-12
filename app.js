80b8ff4dd65fbed3f678a537f3bd0fe22d0736d4
Giovanna Dias <gio.dsdlm@gmail.com>
2018-11-12 01:04:37 -0200

    Criação e configuração da database através do Mongoose.

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//Connect to Mongoose
+mongoose.connect('mongodb://localhost/unideia-dev', {
+   useNewUrlParser: true 
+})  //promise
+.then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err)); 
 
 //Handlebars Middleware
 app.engine('handlebars', exphbs({
   defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
-
-
 
 // Index Route
 app.get('/', (req, res) => {
