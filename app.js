95c767c2161e68fef6dd959f1ae33c4a72180c34
Giovanna Dias <gio.dsdlm@gmail.com>
2018-11-12 20:37:54 -0200

    Instalação e configuração do middleware Body Parser.

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 // Connect to Mongoose
 mongoose.connect('mongodb://localhost/unideia-dev', {
-   useNewUrlParser: true 
-})  //promise
-.then(() => console.log('MongoDB Connected...'))
-.catch(err => console.log(err)); 
+    useNewUrlParser: true
+  }) //promise
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
 
 
 // Load Idea Model
@@ -22,6 +23,14 @@ app.engine('handlebars', exphbs({
   defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
+
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+app.use(bodyParser.json())
+
 
 // Index Route
 app.get('/', (req, res) => {
@@ -41,9 +50,15 @@ app.get('/ideas/add', (req, res) => {
   res.render('ideas/add');
 });
 
+// Processar formulário
+app.post('/ideas', (req, res) => {
+  console.log(req.body);
+  res.send('Ok');
+
+});
 
 
 const port = 5000;
-app.listen(port, ()=>{
+app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
