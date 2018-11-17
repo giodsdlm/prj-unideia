4c8ffa23c5a8de055a636a8724cdff2cd3e1c626
Giovanna <gio.dsdlm@gmail.com>
2018-11-17 05:25:58 -0200

    Validação dos campos dos forms e instalação do bcrypt.

--- routes/users.js
+++ routes/users.js
@@ -13,4 +13,29 @@ router.get('/register', (req, res) =>{
 })
 
 
+//Register Form POST
+router.post('/register', (req, res) => {
+  let errors = [];
+
+  if(req.body.password != req.body.password2){
+    errors.push({text: 'As senhas inseridas não são iguais. '});
+  }
+
+  if(req.body.password.length < 6){
+    errors.push({text:'A senha deve conter pelo menos 6 caracteres.'});
+  }
+
+  if(errors.length > 0) {
+    res.render('users/register', {
+      errors: errors,
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      password2: req.body.password2      
+    });
+  } else {
+    res.send('passed');
+  }
+});
+
 module.exports = router;
